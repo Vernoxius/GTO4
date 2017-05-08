ce536b215abc3b5896bb36a8b698a0fb6bf3efa5
Ruijters,Jeroen J <jeroen.ruijters@student.fontys.nl>
2017-05-08 17:27:17 +0200

    added models to units

--- Assets/Scripts/Unit2.cs
+++ Assets/Scripts/Unit2.cs
@@ -15,6 +15,24 @@ public class Unit2 : Unit {
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (startMoving)
+        {
+            if (pos >= 1)
+            {
+                startMoving = false;
+                doneMoving = true;
+                x = newPosition.x;
+                y = newPosition.y;
+                z = newPosition.z;
+            }
+            this.transform.position = Vector3.Lerp(new Vector3(x, y, z), newPosition, pos);
+            pos += Time.deltaTime;
+        }
+        else if (doneMoving)
+        {
+            GameManager.gameManager.UnselectAll();
+            GameManager.gameManager.SelectUnit(this);
+            doneMoving = false;
+        }
+    }
 }
